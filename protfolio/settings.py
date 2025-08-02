44bdb5b6bbd0db15a8083923f0e137d1b6e3ccf2
auntmaybro <bagriaditya00@gmail.com>
2025-08-02 22:28:15 +0530

    dragon with babies

--- protfolio/settings.py
+++ protfolio/settings.py
@@ -134,12 +134,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
